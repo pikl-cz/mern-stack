01eb2eabf3caa16a36ba2a4690573e93cb72d535
Jan Pikl <jan.pikl@cninvest.cz>
2019-05-30 10:17:12 +0200

    26 - get a delete posts and before

--- config/db.js
+++ config/db.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+//mongoose.set('useFindAndModify', false);
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useCreateIndex: true
+      useCreateIndex: true,
+      useFindAndModify: false
     });
     console.log('MongoDB connected ...');
   } catch (err) {
