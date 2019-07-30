fccc22621861a61b5ddca3615c219b2f93b98eb0
Jan Pikl <jan.pikl@cninvest.cz>
2019-07-30 10:46:17 +0200

    Action create profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+//SOURCE
+//https://github.com/bradtraversy/devconnector_2.0/
+
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
@@ -6,6 +9,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile'
 import PrivateRoute from './components/routing/PrivateRoute';
 
 //Redux
@@ -38,6 +42,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
