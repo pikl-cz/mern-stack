5813ffb2a6d6114f4b2aad3f2b84a5405c91b1d8
Jan Pikl <jan.pikl@cninvest.cz>
2019-08-01 17:57:17 +0200

    Delete education n experiences

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,7 +5,8 @@ import {
     GET_PROFILE,
     PROFILE_ERROR,
     CLEAR_PROFILE,
-    UPDATE_PROFILE
+    UPDATE_PROFILE,
+    ACCOUNT_DELETED
 } from './types';
 
 //Get current users profile
@@ -126,5 +127,67 @@ export const addEducation = (formData, history) => async dispatch => {
             type: PROFILE_ERROR,
             payload: { msg: err.response.statusText, status: err.response.status }
         });
+    }
+}
+
+//Delete experience
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/experience/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Experience removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+
+//Delete education
+export const deleteEducation = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/education/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Education removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+
+//Delete account and profile
+export const deleteAccount = () => async dispatch => {
+    if (window.confirm('Are you sure? This can NOT be undone!')) {
+        try {
+            const res = await axios.delete(`/api/profile/`);
+
+            dispatch({
+                type: CLEAR_PROFILE
+            });
+
+            dispatch({
+                type: ACCOUNT_DELETED
+            });
+
+            dispatch(setAlert('Your account has been permanently deleted', 'success'));
+        } catch (err) {
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: { msg: err.response.statusText, status: err.response.status }
+            });
+        }
     }
 }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,12 +5,13 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import Experience from './Experience';
 import DashboardActions from './DashboardActions';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 import Education from './Education';
 
 
 const Dashboard = ({
     getCurrentProfile,
+    deleteAccount,
     auth: { user },
     profile: { profile, loading }
 }) => {
@@ -28,6 +29,11 @@ const Dashboard = ({
                 <DashboardActions />
                 <Experience experience={profile.experience} />
                 <Education education={profile.education} />
+                <div className="my-2">
+                    <button className="btn btn-danger" onClick={() => deleteAccount()}>
+                        <i className="fas fa-user-minus"></i> Delete my account
+                    </button>
+                </div>
             </Fragment> :
             <Fragment>
                 <p>You have not yet setup a profile, add some info please</p>
@@ -40,6 +46,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired
 };
@@ -49,5 +56,5 @@ const mapStateToProps = state => ({
     profile: state.profile
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(Dashboard);
 
