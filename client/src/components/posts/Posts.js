d9d1a9e066b37684b720a15f01133dcb7a2bf182
Jan Pikl <jan.pikl@cninvest.cz>
2019-08-15 22:14:26 +0200

    GraphQl, next, posty a komentare

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
 import PostItem from './PostItem'
+import PostForm from './PostForm'
 import { getPosts } from '../../actions/post'
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -14,8 +15,8 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
         <p className="lead">
             <i className="fas fa-user"></i> Welcome to the community
         </p>
-        {/* PostForm */}
-        <div className="posts">
+        <PostForm />
+        <div className='posts'>
             {posts.map(post => (
                 <PostItem key={post._id} post={post} />
             ))}
--- server.js
+++ server.js
@@ -26,3 +26,23 @@ app.use('/api/users', require('./routes/api/users'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+
+//GraphQL
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
